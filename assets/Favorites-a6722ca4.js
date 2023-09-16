fd7ded610561ebf74767b24390b5ef5a0dc0a8a7
Ruslan <Giovingo92@gmail.com>
2023-09-16 21:26:33 +0300

    Updates

--- assets/Favorites-a6722ca4.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-157bfd8e.js";function e(){return r.jsx("section",{children:r.jsx("h1",{children:"Favorites"})})}export{e as default};
---
+++ assets/Home-0e2754aa.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-9bbb432b.js";function r(){return e.jsx("section",{children:e.jsx("div",{children:"Home"})})}export{r as default};
