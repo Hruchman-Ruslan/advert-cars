5ac82ff2892f6d4b0add5930147a9061c22e5fc0
Ruslan <Giovingo92@gmail.com>
2023-09-17 01:21:31 +0300

    feat: add title and icon

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,7 @@ import { Container } from "./Home.styled";
 export const HomePage = () => {
   return (
     <Container>
-      <h1>WheelsOnTheGo - Your Ultimate Car Rental Destination</h1>
+      <h1>Wheels On The Go - Your Ultimate Car Rental Destination</h1>
     </Container>
   );
 };
