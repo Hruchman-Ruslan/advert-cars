81691a59de3e1542b6b68cc45ae0d075fcb127d4
Ruslan <Giovingo92@gmail.com>
2023-09-17 02:22:43 +0300

    feat: add styles homePage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,47 @@
-import { Container } from "./Home.styled";
+import { Container, Item, List, Text, Title } from "./Home.styled";
 
 export const HomePage = () => {
   return (
     <Container>
-      <h1>Wheels On The Go - Your Ultimate Car Rental Destination</h1>
+      <Title>Wheels On The Go - Your Ultimate Car Rental Destination</Title>
+      <List>
+        <Item>
+          <Text>Where Every Journey Begins with Ease and Convenience</Text>
+        </Item>
+        <Item>
+          <Text>Welcome to Wheels On The Go</Text>
+        </Item>
+        <Item>
+          <Text>Discover the Freedom of the Open Road</Text>
+        </Item>
+        <Item>
+          <Text>Explore Our Fleet</Text>
+        </Item>
+        <Item>
+          <Text>Choose from a Diverse Range of Premium Vehicles</Text>
+        </Item>
+        <Item>
+          <Text>Why Choose Us?</Text>
+        </Item>
+        <Item>
+          <Text>Unmatched Quality, Unbeatable Service</Text>
+        </Item>
+        <Item>
+          <Text>Reservation Made Easy</Text>
+        </Item>
+        <Item>
+          <Text>Book Your Dream Car in Just a Few Clicks</Text>
+        </Item>
+        <Item>
+          <Text>Contact Us</Text>
+        </Item>
+        <Item>
+          <Text>We are Here to Make Your Journey Memorable</Text>
+        </Item>
+        <Item>
+          <Text>Phone when pressing Rental car😉</Text>
+        </Item>
+      </List>
     </Container>
   );
 };
--- src/components/Home/Home.styled.jsx
+++ src/components/Home/Home.styled.jsx
@@ -1,5 +1,51 @@
 import styled from "@emotion/styled";
+import { getRandomColor } from "../../helpers/randomColor";
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: normal;
+`;
+
+export const Title = styled.h1`
   text-align: center;
+  color: brown;
+  margin-bottom: 50px;
+  font-weight: 900;
+  font-size: 32px;
+`;
+
+export const List = styled.ul`
+  max-width: 1400px;
+  display: flex;
+  flex-direction: column;
+  gap: 35px;
+
+  padding: 0 10px;
+
+  @media (min-width: 768px) {
+    padding: 0 50px;
+  }
+
+  @media (min-width: 1200px) {
+    padding: 0 100px;
+  }
+`;
+
+export const Item = styled.li`
+  text-align: right;
+
+  &:nth-of-type(odd) {
+    text-align: left;
+  }
+`;
+
+export const Text = styled.p`
+  color: #121417;
+  font-size: 24px;
+  font-weight: 600;
+  line-height: calc(20 / 24);
+
+  color: ${getRandomColor};
 `;
