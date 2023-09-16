cef374164a3c6d688927970e2d13618297b63cb6
Ruslan <Giovingo92@gmail.com>
2023-09-17 01:22:56 +0300

    Updates

---
+++ assets/Section.styled-041f7201.js
@@ -0,0 +1,3 @@
+import{n}from"./index-f2032bc6.js";const t=n.section`
+  padding: 50px 0;
+`;export{t as S};
--- assets/Section.styled-662f4342.js
+++
@@ -1,3 +0,0 @@
-import{n}from"./index-15d03750.js";const t=n.section`
-  padding: 50px 0;
-`;export{t as S};
