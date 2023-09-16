484dd94b5fb3b91ed5a8c103f8a0ee01b64e6249
Ruslan <Giovingo92@gmail.com>
2023-09-17 01:03:13 +0300

    Updates

--- assets/Home-0e2754aa.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-9bbb432b.js";function r(){return e.jsx("section",{children:e.jsx("div",{children:"Home"})})}export{r as default};
---
+++ assets/Home-5cfdc0a9.js
@@ -0,0 +1,3 @@
+import{n as t,j as e}from"./index-15d03750.js";import{S as n}from"./Section.styled-662f4342.js";const r=t.div`
+  text-align: center;
+`,o=()=>e.jsx(r,{children:e.jsx("h1",{children:"WheelsOnTheGo - Your Ultimate Car Rental Destination"})});function a(){return e.jsx(n,{children:e.jsx(o,{})})}export{a as default};
